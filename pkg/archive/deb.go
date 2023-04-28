672ad911ceca60149b9236e02d842ce91594e641
Peter Engelbert <pmengelbert@gmail.com>
2023-04-28 11:07:36 -0400

    Revert deb.go
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- pkg/archive/deb.go
+++ pkg/archive/deb.go
@@ -3,6 +3,7 @@ package archive
 import (
 	"bytes"
 	"fmt"
+	"path/filepath"
 	"strings"
 	"text/template"
 
@@ -127,21 +128,62 @@ func (d *DebPackager) withInstallScripts(c *dagger.Container) (*dagger.Container
 
 	for i := range d.a.InstallScripts {
 		script := d.a.InstallScripts[i]
-		filename := script.Script
-		flag := ""
-		switch script.When {
-		case PkgActionPostInstall, PkgActionUpgrade:
-			flag = flagPostInstall
-		case PkgActionPreRemoval:
-			flag = flagPreRm
-		case PkgActionPostRemoval:
-			flag = flagPostRm
-		default:
-			panic("unrecognized package action: " + fmt.Sprintf("%s", script.When))
-		}
-		newArgs = append(newArgs, flag, filename)
+		var a []string
+		c, a = d.installScript(&script, c)
+		newArgs = append(newArgs, a...)
 	}
 
+	return c, newArgs
+}
+
+func (d *DebPackager) installScript(script *InstallScript, c *dagger.Container) (*dagger.Container, []string) {
+	newArgs := []string{}
+
+	var templateStr, filename, flag string
+	switch script.When {
+	case PkgActionPostInstall, PkgActionUpgrade:
+		filename = filenamePostInstall
+		flag = flagPostInstall
+		templateStr = `
+if [ "$1" = "configure" ] || [ "$1" = "abort-upgrade" ] || [ "$1" = "abort-deconfigure" ] || [ "$1" = "abort-remove" ] ; then
+  {{ replace .Script "\n" "\n  " }}
+fi
+            `
+	case PkgActionPreRemoval:
+		filename = filenamePreRm
+		flag = flagPreRm
+		templateStr = `
+if [ "$1" = remove ]; then
+  {{ replace .Script "\n" "\n  " }}
+fi
+            `
+	case PkgActionPostRemoval:
+		filename = filenamePostRm
+		flag = flagPostRm
+		templateStr = `
+if [ "$1" = "purge" ]; then
+  {{ replace .Script "\n" "\n  " }}
+fi
+            `
+	default:
+		panic("unrecognized package action: " + fmt.Sprintf("%d", script.When))
+	}
+
+	filename = filepath.Join("/build", filename)
+
+	tpl, err := template.New("installScript").Funcs(template.FuncMap{"replace": strings.ReplaceAll}).Parse(templateStr)
+	if err != nil {
+		panic(err)
+	}
+
+	buf := new(bytes.Buffer)
+	err = tpl.Execute(buf, script)
+	if err != nil {
+		panic(err)
+	}
+
+	c = c.WithNewFile(filename, dagger.ContainerWithNewFileOpts{Contents: buf.String()})
+	newArgs = append(newArgs, flag, filename)
 	return c, newArgs
 }
 
